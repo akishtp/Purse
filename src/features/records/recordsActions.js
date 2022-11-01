3a057d55ec0cadd268306db21944babbf2b54d53
akishtp <makishtp@gmail.com>
2022-11-01 19:36:56 +0530

    apparently the local server was running last time 😔

--- src/features/records/recordsActions.js
+++ src/features/records/recordsActions.js
@@ -5,8 +5,9 @@ export const getRecords = createAsyncThunk(
   "record/get",
   async (_, { rejectWithValue }) => {
     try {
-      const { data } = await axios.get("records");
-      // console.log(data);
+      const { data } = await axios.get(
+        "https://verlow-server.up.railway.app/api/records"
+      );
       return data;
     } catch (error) {
       return rejectWithValue(error.response.data.error);
@@ -27,7 +28,7 @@ export const addRecord = createAsyncThunk(
     };
     try {
       const { data } = await axios.post(
-        "records",
+        "https://verlow-server.up.railway.app/api/records",
         { type, account, amount, category, date, payee, note },
         config
       );
@@ -41,14 +42,16 @@ export const addRecord = createAsyncThunk(
 export const deleteRecord = createAsyncThunk(
   "record/delete",
   async (_id, { rejectWithValue }) => {
-    console.log(_id);
     const config = {
       headers: {
         "Content-Type": "application/json",
       },
     };
     try {
-      const { data } = await axios.delete(`records/${_id}`, config);
+      const { data } = await axios.delete(
+        `https://verlow-server.up.railway.app/api/records/${_id}`,
+        config
+      );
       return data;
     } catch (error) {
       return rejectWithValue(error.response.data.error);
