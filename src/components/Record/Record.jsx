d989ea860922252c22a0aa36e35362336bb0b8ad
akishtp <makishtp@gmail.com>
2023-01-03 09:22:39 +0530

    oppa button actions style ㉿

--- src/components/Record/Record.jsx
+++ src/components/Record/Record.jsx
@@ -2,25 +2,31 @@ import "./Record.css";
 
 const Record = ({ record }) => {
   return (
-    <div className="record">
-      <div className="left-side">
-        <img
-          src={require(`../../Assets/${record.category}.png`)}
-          alt="record-category"
-        />
-        <div className="record-details">
-          <p className="category">{record.category}</p>
-          <p className="account">{record.account}</p>
-          <div className="extras">
-            {record.note && <p className="payee">{record.note}</p>}
-            {record.note && record.payee && <>&nbsp;-&nbsp;</>}
-            {record.payee && <p className="note">{record.payee}</p>}
+    <div className="record-inner-wrapper">
+      <div className="record">
+        <div className="left-side">
+          <img
+            src={require(`../../Assets/${record.category}.png`)}
+            alt="record-category"
+          />
+          <div className="record-details">
+            <p className="category">{record.category}</p>
+            <p className="account">{record.account}</p>
+            <div className="extras">
+              {record.note && <p className="payee">{record.note}</p>}
+              {record.note && record.payee && <>&nbsp;-&nbsp;</>}
+              {record.payee && <p className="note">{record.payee}</p>}
+            </div>
           </div>
         </div>
+        <div className={`right-side ${record.type}`}>
+          {record.type === "Expense" ? "-" : "+"}
+          {record.amount}
+        </div>
       </div>
-      <div className={`right-side ${record.type}`}>
-        {record.type === "Expense" ? "-" : "+"}
-        {record.amount}
+      <div className="record-actions">
+        <button className="edit">✍️</button>
+        <button className="delete">🗑️</button>
       </div>
     </div>
   );
