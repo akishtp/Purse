a14e4021e7242e04e4db4efec7de6597a2951e2f
akishtp <makishtp@gmail.com>
2023-12-01 08:56:14 +0530

    ❌: I am my own entity

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,13 +1,14 @@
 import '@/app/ui/global.css';
+import { playfair_display } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
-}: {
+} : {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${playfair_display.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,6 +2,6 @@ import Link from 'next/link';
 
 export default function Page() {
   return (
-    <main className='bg-black'>Hey</main>
+    <main className=''>Hey</main>
   );
 }
