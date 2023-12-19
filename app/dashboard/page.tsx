71f0235bdaba69e8c8c9ccc3da4a27dede599475
akishtp <makishtp@gmail.com>
2023-12-19 17:13:01 +0530

    V2

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { playfair_display } from "../ui/fonts";
-
-export default async function Page() {
-  return (
-    <main className="bg-neutral-900 min-h-full rounded-xl">
-      <h1 className={`${playfair_display.className}`}>Dashboard</h1>
-    </main>
-  );
-}
--- app/dashboard/records/page.tsx
+++
@@ -1,9 +0,0 @@
-import { fetchRecords } from "@/app/lib/data";
-// import { Record } from "../../lib/definitions";
-
-export default async function Page() {
-  const records = await fetchRecords();
-  console.log(records);
-
-  return <p>Hello</p>;
-}
