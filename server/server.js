b2c0f7a6261a9bb8f8771ae151a1d5e6c4e4db08
akishtp <makishtp@gmail.com>
2023-12-25 19:07:35 +0530

    init server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
