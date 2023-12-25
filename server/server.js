e5692ce3373e6e1a8ce137ae2e853916c4ca3fd3
akishtp <makishtp@gmail.com>
2023-12-25 20:29:06 +0530

    backend login seeming works

--- server/server.js
+++ server/server.js
@@ -3,9 +3,31 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
+app.options("*", cors());
+
 const mongoose = require("mongoose");
 
+const passport = require("passport");
+const passportSetup = require("./passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
+var cookieSession = require("cookie-session");
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.COOKIE_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
+
 app.use(express.json());
+
+const authRouter = require("./routes/auth");
+
+app.use("/auth", authRouter);
 
 mongoose
   .connect(process.env.MONGO_URI)
