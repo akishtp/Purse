12215ed2940a802b3398e6e1e07832599c847f31
Akish tp <makishtp@gmail.com>
2024-01-03 15:28:05 +0530

    frontend connection with backend connection 💍

--- server/services/user.go
+++ server/services/user.go
@@ -21,6 +21,12 @@ func CreateUser(db *sql.DB) http.HandlerFunc {
 		var u User
 		json.NewDecoder(r.Body).Decode(&u)
 
+// check if user with same name exists
+		name := db.QueryRow("SELECT * FROM users WHERE name = $1", u.Name)
+		if name != nil {
+			log.Fatal("name already used")
+		}
+
 		err := db.QueryRow("INSERT INTO users (name, password) VALUES ($1, $2) RETURNING id", u.Name, u.Password).Scan(&u.Id)
 		if err != nil {
 			log.Fatal(err)
