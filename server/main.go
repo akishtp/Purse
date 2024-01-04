992e9297d5d2d2585d304d9369583891c2c8f4e3
Akish tp <makishtp@gmail.com>
2024-01-04 23:39:36 +0530

    CORS will be the death of me

--- server/main.go
+++ server/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/akishtp/purse/database"
 	"github.com/akishtp/purse/middleware"
 	"github.com/akishtp/purse/models"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -33,9 +32,10 @@ func loadEnv() {
 }
 
 func serveApplication() {
-    router := gin.Default()
+    router := gin.New()
 
-	router.Use(cors.Default())
+	// router.Use(cors.Default())
+	router.Use(CORSMiddleware())
 
     publicRoutes := router.Group("/auth")
     publicRoutes.POST("/signup", controller.Signup)
@@ -44,10 +44,22 @@ func serveApplication() {
 	protectedRoutes := router.Group("/api")
     protectedRoutes.Use(middleware.JWTAuthMiddleware())
     protectedRoutes.POST("/account", controller.AddNewAccount)
-    protectedRoutes.GET("/account", controller.GetUserAccounts)
+    protectedRoutes.GET("/accounts", controller.GetUserAccounts)
 
     router.Run(":8000")
     fmt.Println("Server running on port 8000")
 }
 
-// jwt:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYXQiOjE3MDQzODE4MjksImlhdCI6MTcwNDM4MTgyOSwiaWQiOjF9.Lkx_HaYrqFhLUZTnXJMgjgIHyKuUFLpA49CkJ1b6bmo"
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Header("Access-Control-Allow-Origin", "*")
+        c.Header("Access-Control-Allow-Credentials", "true")
+        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+        c.Next()
+    }
+}
