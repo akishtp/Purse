a953441085a86f8beddb9749ad5d8fa0538433a2
Akish tp <makishtp@gmail.com>
2024-01-04 22:51:45 +0530

    Accounts in Server

--- server/main.go
+++ server/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/akishtp/purse/controller"
 	"github.com/akishtp/purse/database"
+	"github.com/akishtp/purse/middleware"
 	"github.com/akishtp/purse/models"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -40,6 +41,13 @@ func serveApplication() {
     publicRoutes.POST("/signup", controller.Signup)
     publicRoutes.POST("/login", controller.Login)
 
+	protectedRoutes := router.Group("/api")
+    protectedRoutes.Use(middleware.JWTAuthMiddleware())
+    protectedRoutes.POST("/account", controller.AddNewAccount)
+    protectedRoutes.GET("/account", controller.GetUserAccounts)
+
     router.Run(":8000")
     fmt.Println("Server running on port 8000")
 }
+
+// jwt:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYXQiOjE3MDQzODE4MjksImlhdCI6MTcwNDM4MTgyOSwiaWQiOjF9.Lkx_HaYrqFhLUZTnXJMgjgIHyKuUFLpA49CkJ1b6bmo"
--- server/models/account.go
+++ server/models/account.go
@@ -1,9 +1,23 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/akishtp/purse/database"
+	"gorm.io/gorm"
+)
 
 type Account struct{
 	gorm.Model
-	AccountName string `gorm:"type:text" json:"account_name"`
-	UserId 		uint
+	AccountName string 	`gorm:"not null;" json:"account_name"`
+	Balance 	int		`gotm:"not null;" json:"balance"`
+	Color		string	`gorm:"not null;" json:"account"`
+	UserID 		uint
+}
+
+func (account *Account) Save() (*Account, error){
+	err := database.Database.Create(&account).Error
+
+	if err != nil {
+        return &Account{}, err
+    }
+	return account, nil
 }
