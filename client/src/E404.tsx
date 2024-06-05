7f8cbd4ceba226d4ed15936a72f7dd312d00b3e2
Akish T P <makishtp@gmail.com>
2024-06-05 20:37:01 +0530

    🚀: init

--- client/src/E404.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouteError } from "react-router-dom";
-
-const E404 = () => {
-  const error: any = useRouteError();
-  return <div>{error.statusText || error.message}</div>;
-};
-
-export default E404;
--- server/models/authenticationInput.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type AuthenticationInput struct{
-	Name		string `json:"name" binding:"required"`
-	Password	string `json:"password" binding:"required"`
-}
